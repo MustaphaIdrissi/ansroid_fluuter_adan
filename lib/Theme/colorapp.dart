346deb7c535f7314b46c418cae63200cf6742994
Mustapha <56976902+MustaphaIdrissi@users.noreply.github.com>
2024-02-20 18:58:16 +0100

    Add files via upload
    x

---
+++ lib/Theme/colorapp.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorApp {
+  Color background = Color(0xFF80BCBD); // Hexadecimal color value
+  Color background2 = Color(0xFFAAD9BB); // Hexadecimal color value
+  Color Textcolor = Color(0xFFFFFFFF); // Hexadecimal color value
+  Color Textcolor2 = Color.fromARGB(255, 0, 0, 0);
+  Color trans = Colors.transparent; // Hexadecimal color value
+
+  Color shado1 = Colors.grey.withOpacity(0.5);
+}
